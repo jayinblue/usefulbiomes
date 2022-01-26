143abf7de9469fcdc9c511206c3a3682cf50d3af
BuildTools <unconfigured@null.spigotmc.org>
2022-01-25 23:11:07 -0500

    Initial commit

---
+++ src/main/java/net/jayinblue/usefulbiomes/UsefulBiomes.java
@@ -0,0 +1,12 @@
+package net.jayinblue.usefulbiomes;
+
+import net.fabricmc.api.ModInitializer;
+
+public class UsefulBiomes implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+
+
+	}
+}
